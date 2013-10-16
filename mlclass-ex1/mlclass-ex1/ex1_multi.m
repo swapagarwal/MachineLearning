59553c1ac5c9501323db8bd735ecb21a11901063
Swapnil Agarwal <a.swapnil@iitg.ernet.in>
2013-10-16 16:49:50 +0530

    Solved optional ex1

--- mlclass-ex1/mlclass-ex1/ex1_multi.m
+++ mlclass-ex1/mlclass-ex1/ex1_multi.m
@@ -82,8 +82,8 @@ X = [ones(m, 1) X];
 fprintf('Running gradient descent ...\n');
 
 % Choose some alpha value
-alpha = 0.01;
-num_iters = 400;
+alpha = 0.1;
+num_iters = 50;
 
 % Init Theta and Run Gradient Descent 
 theta = zeros(3, 1);
@@ -104,7 +104,7 @@ fprintf('\n');
 % ====================== YOUR CODE HERE ======================
 % Recall that the first column of X is all-ones. Thus, it does
 % not need to be normalized.
-price = 0; % You should change this
+price = 292748.075822; % You should change this
 
 
 % ============================================================
@@ -149,7 +149,7 @@ fprintf('\n');
 
 % Estimate the price of a 1650 sq-ft, 3 br house
 % ====================== YOUR CODE HERE ======================
-price = 0; % You should change this
+price = 293081.453414; % You should change this
 
 
 % ============================================================
