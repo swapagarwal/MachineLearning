afc68b4da74daa6e072744c71b709ce4ef9802f7
Swapnil Agarwal <a.swapnil@iitg.ernet.in>
2013-10-16 15:44:34 +0530

    Finally, featureNormalize works!
    Added .gitignore for ml_login_data.mat

--- mlclass-ex1/mlclass-ex1/featureNormalize.m
+++ mlclass-ex1/mlclass-ex1/featureNormalize.m
@@ -25,11 +25,11 @@ sigma = zeros(1, size(X, 2));
 %
 % Hint: You might find the 'mean' and 'std' functions useful.
 %       
-
-
-
-
-
+for iter = 1:size(X, 2)
+	mu(iter) = mean(X(:,iter));
+	sigma(iter) = std(X(:,iter));
+	X_norm(:,iter) = (X(:,iter) - mu(iter)) / sigma(iter);
+end
 
 
 
